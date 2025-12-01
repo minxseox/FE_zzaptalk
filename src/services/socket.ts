57dfc8ffc21b7a8388334a64c999c10ba6332328
minseo <gimminseo288@gamil.com>
2025-12-02 03:26:36 +0900

    소켓 엔진엑스 수정

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -18,7 +18,7 @@ function resolveWsUrl(): string {
   }
 
   const protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
-  WS_URL = `${protocol}//${window.location.host}/ws/websocket`;
+  WS_URL = `${protocol}//${window.location.host}/ws`;
   console.log("[Socket] WS_URL:", WS_URL);
   return WS_URL;
 }
